da9de6ef1120c6f4531196bc2021db290cdd968c
sammie287 <kyleoneill24@gmail.com>
2017-11-01 15:49:55 -0400

    First update, done on 11-1

--- P3/main.c
+++ P3/main.c
@@ -1,9 +1,15 @@
 ﻿#include <stdio.h>
+#include <string.h>
 
 int main (int argc, char *argv[])
 {
-	printf ("Hello world!\n");
-	
-	return 0;
-}
+	while(1){
+		char command[4];
+		int intValue;
+		char stringValue[255];
 
+		printf("Command? ");
+		fflush(stdout);
+		scanf("%s %d %s",command, &intValue, stringValue);
+	}
+}
