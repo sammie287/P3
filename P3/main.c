bc00bbf52de654a96db975567573ea5033a07e86
sammie287 <kyleoneill24@gmail.com>
2017-11-03 11:36:38 -0400

    Transfer from laptop to dekstop

--- P3/main.c
+++ P3/main.c
@@ -11,5 +11,18 @@ int main (int argc, char *argv[])
 		printf("Command? ");
 		fflush(stdout);
 		scanf("%s %d %s",command, &intValue, stringValue);
+
+		if(!strcmp(command, "ina"))
+			;//Run insert after method here
+		else if(!strcmp(command, "inb"))
+			;//Run insert before method here
+		else if(!strcmp(command, "del"))
+			;//Run delete method here
+		else if(!strcmp(command, "rep"))
+			;//Run replace method here
+		else if(!strcmp(command, "prn"))
+			;//Run print method here
+		else if(!strcmp(command, "end"))
+			;//Run end method here? Maybe just a break
 	}
 }
